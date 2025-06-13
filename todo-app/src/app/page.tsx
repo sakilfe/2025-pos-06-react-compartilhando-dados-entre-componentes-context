a968e29040a8326d849f7d7eebbb7ae06f6aa43c
ssf! <123976455+sakilfe@users.noreply.github.com>
2025-06-13 17:00:07 +0000

    atualizando

--- todo-app/src/app/page.tsx
+++ todo-app/src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <h1 className="text-2xl font-bold mb-4 text-white">Página Inicial</h1>
       <Link
         href="/tarefas"
-        className="bg-amber-500 hover:bg-amber-400 text-black px-4 py-2 rounded cursor-pointer"
+        className="bg-purple-700 hover:bg-purple-600 text-black px-4 py-2 rounded cursor-pointer"
       >
         Ir para Tarefas
       </Link>
